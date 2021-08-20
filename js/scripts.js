d8cb5e8d11f1cc88b10300814b40cda5ceeea355
igregorioneto <igregoriodoneto@outlook.com>
2021-08-19 22:30:41 -0300

    Responsividade e iniciando a criação dos itens sendo passado com js

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,29 @@ let i = setInterval(function () {
   document.getElementById('conteudo').style.display = 'block'
 
 }, 2000)
+
+let postagem = [
+  {
+    "data": "02 de julho, 2021",
+    "titulo": "Agora é oficial: O Windows 11 está vindo",
+    "descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sapien neque, pretium quis molestie at,tincidunt et velit. Interdum et malesuada fames ac ante ipsum primis in faucibus."
+  },
+  {
+    "data": "05 de agosto, 2021",
+    "titulo": "Estilos com CSS",
+    "descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sapien neque, pretium quis molestie at,tincidunt et velit."
+  },
+  {
+    "data": "18 de agosto, 2021",
+    "titulo": "Construindo site com HTML, CSS e Javascript",
+    "descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sapien neque, pretium quis molestie at,tincidunt et velit. Interdum et malesuada fames ac ante ipsum primis in faucibus."
+  }
+]
+
+let post = document.querySelector('.card-post')
+let data = document.querySelector('.date')
+let informacao_post = document.querySelector('.container-post')
+
+for (let p of postagem) {
+  console.log(p)
+}
